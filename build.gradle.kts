e1fb222051b1df2e76feab64d26375fac0dd94dd
Piero Rafael <pierobecerra19@gmail.com>
2026-04-02 21:49:31 -0500

    implementation of jib for generation images OCI Distroless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import com.google.cloud.tools.jib.gradle.JibExtension
+
 plugins {
     id("org.springframework.boot") version "3.4.1" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
+    id("com.google.cloud.tools.jib") version "3.4.4" apply false
     java
 }
 
@@ -17,6 +20,19 @@ subprojects {
     apply(plugin = "java")
     apply(plugin = "io.spring.dependency-management")
     apply(plugin = "org.springframework.boot")
+    apply(plugin = "com.google.cloud.tools.jib")
+
+    configure<JibExtension> {
+        from {
+            image = "gcr.io/distroless/java21-debian12"
+        }
+        to {
+            image = "cloudnative/${project.name}:${project.version}"
+        }
+        container {
+            jvmFlags = listOf("-XX:+UseZGC", "-XX:MaxRAMPercentage=75.0")
+        }
+    }
 
     java {
         toolchain {
